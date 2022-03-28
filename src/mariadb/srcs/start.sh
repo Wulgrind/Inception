da48087abb60037f3b309d39133dfe3613f221eb
Quentin Brillais <qbrillai@c2r5p7.42nice.fr>
2022-03-28 19:21:51 +0200

    new commit

--- src/mariadb/srcs/start.sh
+++ src/mariadb/srcs/start.sh
@@ -1,15 +1,14 @@
-#SSL
+#mysql_install_db --defaults-file=~/.my.cnf
 
-mkdir /etc/nginx/ssl
-openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out /etc/nginx/ssl/localhost.pem -keyout /etc/nginx/ssl/localhost.key -subj "/C=FR/ST=Nice/L=Nice/O=42 School/OU=quentin/CN=localhost"
+#userdel mysql
+#useradd -u 999 mysql
+service mysql start
 
-#NGINX
+#echo "GRANT ALL ON *.* TO 'quentin'@'localhost' IDENTIFIED BY '123'" | mysql -u root
+#echo "FLUSH PRIVILEGES;" | mysql -u root
 
-mkdir var/www/localhost
-mv ./default etc/nginx/sites-available
-ln -s etc/nginx/sites-available/default etc/nginx/sites-enabled
-chown -R www-data /var/www/*
-chmod -R 755 /var/www/*
+#echo FLUSH PRIVILEGES;
 
-service nginx start
-#sleep infinity
+#service mysql restart
+sleep infinity
+#exec mysqld --user=mysql --console
---
+++ src/wordpress/srcs/start.sh
@@ -0,0 +1,24 @@
+#MYSQL SERVICES
+
+service mysql start
+echo "CREATE DATABASE wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;" | mysql -u root
+echo "GRANT ALL ON wordpress.* TO 'wordpress_user'@'localhost' IDENTIFIED BY 'password';" | mysql -u root
+echo "FLUSH PRIVILEGES;" | mysql -u root
+
+
+#PHP SERVICES
+
+wget https://files.phpmyadmin.net/phpMyAdmin/4.9.0.1/phpMyAdmin-4.9.0.1-all-languages.tar.gz
+tar xvf phpMyAdmin-4.9.0.1-all-languages.tar.gz
+mv phpMyAdmin-4.9.0.1-all-languages var/www/localhost/phpmyadmin
+mv ./config.inc.php var/www/localhost/phpmyadmin
+chmod 660 /var/www/localhost/phpmyadmin/config.inc.php
+chown -R www-data:www-data /var/www/localhost/phpmyadmin
+service php7.3-fpm start
+echo "GRANT ALL ON *.* TO 'quentin'@'localhost' IDENTIFIED BY '123'" | mysql -u root
+echo "FLUSH PRIVILEGES;" | mysql -u root
+
+cp info.php /var/www/localhost/info.php
+service php7.3-fpm restart
+
+exec mysqld --user=mysql --console
