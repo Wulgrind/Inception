45685a55dc7f5e395d1468e74540aeea74c4c11d
Quentin Brillais <qbrillai@c2r5p7.42nice.fr>
2022-04-26 15:49:56 +0200

    new commit

--- src/wordpress/srcs/start.sh
+++ src/wordpress/srcs/start.sh
@@ -1,7 +1,5 @@
 echo "clear_env=false" >> etc/php7/php-fpm.conf
 mv ./wp-config.php /var/www/localhost/wordpress/
-# wget https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1.php
-# mv ./adminer-4.8.1.php /var/www/localhost/wordpress/adminer.php
 wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
